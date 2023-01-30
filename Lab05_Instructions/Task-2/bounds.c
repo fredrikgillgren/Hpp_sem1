ac893009d96c8266f8c4c56d4bcd588b05204afd
JohanRensfeldt <johan.rensfeldt@gmail.com>
2023-01-30 13:27:11 +0100

    3

--- Lab05_Instructions/Task-2/bounds.c
+++ Lab05_Instructions/Task-2/bounds.c
@@ -9,7 +9,6 @@ int foo(int j, int size);
 
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
     const int size = 16; 
     const int numiters = 100000000;
     int i,j;
@@ -22,20 +21,6 @@ int main(int argc, char **argv)
        {
           printf("Error: Index out of range");
        }
-=======
-   const int size = 16;
-   const int numiters = 100000000;
-   int i, j;
-   double list[size];
-   for (j = 0; j < numiters; j++)
-   {
-      i = foo(j, size);
-#if FAST
-      if ((unsigned int)i >= (unsigned int)size)
-      {
-         printf("Error: Index out of range");
-      }
->>>>>>> ae718f709779a651a188820c9e4d83e68f8e8f28
 #else
       if (i < 0 || i >= size)
       {
