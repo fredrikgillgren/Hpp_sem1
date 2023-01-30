527bb24a3036937801826473b2036c63703406bd
Gustav Fredrikson <gustav.fredrikson@gmail.com>
2023-01-30 12:50:45 +0100

    added files

--- Lab05_Instructions 2/Task-3/norms.c
+++ Lab05_Instructions 2/Task-3/norms.c
@@ -12,13 +12,13 @@ int main(int argc, char **argv)
       variable in such a way that it is (almost) restored to the same
       value again in each loop iteration. The value will probably
       change slightly due to rounding errors. */
-   for (i=0; i<5000000; i++)
+   for (i = 0; i < 5000000; i++)
    {
-      tiny += tiny/2.0;
+      tiny += tiny / 2.0;
       tiny *= 0.666667;
-      tiny += tiny/2.0;
+      tiny += tiny / 2.0;
       tiny *= 0.666667;
-      tiny += tiny/2.0;
+      tiny += tiny / 2.0;
       tiny *= 0.666666;
    }
    printf("%e\n", tiny);
