24206983339e8c5b8904fd53fd3e8e80bcdd0453
Gustav Fredrikson <gustav.fredrikson@gmail.com>
2023-01-30 12:59:39 +0100

    Did task 3

--- Lab05_Instructions/Task-3/denorms.c
+++ Lab05_Instructions/Task-3/denorms.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
    int i;
-   float tiny = 1e-39;
+   float tiny = 1e-49;
 
    printf("%e\n", tiny);
 
@@ -12,13 +12,13 @@ int main(int argc, char **argv)
       variable in such a way that it is (almost) restored to the same
       value again in each loop iteration. The value will probably
       change slightly due to rounding errors. */
-   for (i=0; i<5000000; i++)
+   for (i = 0; i < 5000000; i++)
    {
-      tiny += tiny/2.0;
+      tiny += tiny / 2.0;
       tiny *= 0.666667;
-      tiny += tiny/2.0;
+      tiny += tiny / 2.0;
       tiny *= 0.666667;
-      tiny += tiny/2.0;
+      tiny += tiny / 2.0;
       tiny *= 0.666666;
    }
    printf("%e\n", tiny);
