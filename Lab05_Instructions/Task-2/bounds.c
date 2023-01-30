ae718f709779a651a188820c9e4d83e68f8e8f28
Gustav Fredrikson <gustav.fredrikson@gmail.com>
2023-01-30 13:19:23 +0100

    Task 2 done

--- Lab05_Instructions/Task-2/bounds.c
+++ Lab05_Instructions/Task-2/bounds.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define FAST 0
+#define FAST 1
 
 /* From A. Fog: Example 14.4. */
 
@@ -9,33 +9,34 @@ int foo(int j, int size);
 
 int main(int argc, char **argv)
 {
-    const int size = 16; 
-    const int numiters = 100000000;
-    int i,j;
-    double list[size];
-    for(j = 0; j<numiters; j++)
-    {
-       i = foo(j,size);
+   const int size = 16;
+   const int numiters = 100000000;
+   int i, j;
+   double list[size];
+   for (j = 0; j < numiters; j++)
+   {
+      i = foo(j, size);
 #if FAST
-       if (0 /*put faster array bounds checking here*/) 
-       {
-          printf("Error: Index out of range");
-       }
+      if ((unsigned int)i >= (unsigned int)size)
+      {
+         printf("Error: Index out of range");
+      }
 #else
-       if (i < 0 || i >= size) 
-       {
-          printf("Error: Index out of range");
-       }
+      if (i < 0 || i >= size)
+      {
+         printf("Error: Index out of range");
+      }
 #endif
-       else {
-          list[i] += 1.0;
-       }
-    }
-    return 0;
+      else
+      {
+         list[i] += 1.0;
+      }
+   }
+   return 0;
 }
 
 /* Pretend you don't know the definition of this function. */
 int foo(int j, int size)
 {
-	return j%size;
+   return j % size;
 }
